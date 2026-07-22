3a1b35a726afbb83665721df16b637c4155a6101
lucianohamilton85 <nestelineluciano@gmail.com>
2026-07-22 13:04:09 +0200

    encore  pour Hostinger

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Assure des chemins relatifs simples et universels
+  build: {
+    // Force Vite à sortir les fichiers directement à la racine du dossier dist
+    assetsDir: '', 
+    rollupOptions: {
+      output: {
+        // Enlève le préfixe "assets/" des fichiers JS et CSS
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash].[ext]'
+      }
+    }
+  }
 })
